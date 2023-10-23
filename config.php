10d557fbcfd0400a672f4cfa7131e6a608c36509
root <root@ubuntu.ubuntu>
2023-10-23 02:13:56 -0400

    init

---
+++ config.php
@@ -0,0 +1,16 @@
+<?php
+
+define('APP_DEBUG', false); // 调试模式开关
+
+define('DATABASE_HOSTNAME', 'mysql'); // 数据库服务器地址
+define('DATABASE_NAME', 'cloud'); // 数据库名
+define('DATABASE_USERNAME', 'cloud'); // 数据库用户名
+define('DATABASE_PASSWORD', 'Ka8mS5DkYZQk4XYh'); // 数据库密码
+define('DATABASE_PORT', '3306'); // 数据库端口
+define('DATABASE_CHARSET', 'utf8mb4'); // 数据库编码
+
+define('DIR_ADMIN', 'vabugjtq'); // 后台目录
+
+define('AUTHCODE', 'j5MQ1F74FuJj6Pw68h'); //系统唯一身份验证字符
+
+define('IS_ZKEYS', false); //是否zkeys迁移系统
